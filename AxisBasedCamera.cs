daabd1f0be99ea97be980327e30fa38105a6603f
Spencer Pauly <spencerpauly@gmail.com>
2019-02-28 08:58:51 -0600

    Add files via upload

---
+++ AxisBasedCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisBasedCamera : PlayerCamera {
+
+    private Vector3 desiredPosition;
+    private Vector3 playerViewOffset;    
+    private Vector3 offset;
+
+    void Start() {
+        offset = new Vector3(0, cameraDistance * Mathf.Sin(Mathf.Deg2Rad*cameraAngle), -cameraDistance * Mathf.Cos(Mathf.Deg2Rad*cameraAngle));
+        playerViewOffset = new Vector3(0.0f, cameraOffset, 0.0f);
+    }
+
+    void LateUpdate() {
+        if (InputManager.SwipeLeft()) {
+            RotateCamera(-cameraRotateStepAngle);
+        } else if (InputManager.SwipeRight()) {
+            RotateCamera(cameraRotateStepAngle);
+        }
+
+        //This WIP code is if you want a multidimensional follow camera that's not axis limited
+
+        //float diffAngle = playerMovement.transform.eulerAngles.y - VectorPlus.VectorToDegree(GetLookDirection());
+        //offset = Quaternion.Euler(0, diffAngle, 0) * offset;
+        //Debug.Log(diffAngle, this);
+
+        //
+
+        desiredPosition = playerMovement.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, cameraSmoothness * Time.deltaTime);
+        transform.LookAt(playerMovement.transform.position + playerViewOffset);
+    }
+
+    public void RotateCamera(float angle)
+    {
+        offset = Quaternion.Euler(0, angle, 0) * offset;
+    }
+
+    
+}
---
+++ PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerCamera : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+
+    [Range(0, 90)]
+    public int cameraAngle;
+    [Range(2, 40)]
+    public float cameraDistance;
+    [Range(0, 20)]
+    public float cameraOffset;
+    [Range(0, 180)]
+    public int cameraRotateStepAngle;
+    [Range(0, 20)]
+    public float cameraSmoothness;
+
+    public Vector3 GetLookDirection() {
+        return VectorPlus.DegreeToVector(transform.eulerAngles.y);
+    }
+
+}
